5bd51bb6835f7cc7e99a1fbd719ba38e61e8aa14
hadreemustaffa <hadree.mustaffa@gmail.com>
2024-06-24 16:12:32 +0800

    style: add sections styling

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const drawer = document.getElementById('navDrawer');
 const toggleButton = document.getElementById('toggleBtn');
 
+const faqContentWrapper = document.querySelector(
+  '.container--faqs .container__content-wrapper'
+);
+const faqButton = document.getElementById('faqButton');
+
 toggleButton.addEventListener('click', () => {
   const isExpanded = toggleButton.getAttribute('aria-expanded') === 'true';
 
@@ -10,5 +15,18 @@ toggleButton.addEventListener('click', () => {
   } else {
     toggleButton.ariaExpanded = 'false';
     drawer.removeAttribute('style');
+  }
+});
+
+faqButton.addEventListener('click', () => {
+  if (faqButton.getAttribute('aria-expanded') == 'false') {
+    faqButton.setAttribute('aria-expanded', 'true');
+    faqContentWrapper.classList.remove('collapse');
+    faqContentWrapper.classList.add('show');
+  } else {
+    faqButton.setAttribute('aria-expanded', 'false');
+    faqContentWrapper.removeAttribute('style');
+    faqContentWrapper.classList.remove('show');
+    faqContentWrapper.classList.add('collapse');
   }
 });
