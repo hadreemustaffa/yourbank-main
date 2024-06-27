3bc6390904e1ed0579361bba83b96ad234ea2c06
hadreemustaffa <hadree.mustaffa@gmail.com>
2024-06-27 12:56:15 +0800

    chore: add base path and resolve build path

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,19 @@
 import { defineConfig } from 'vite';
+import { resolve } from 'path';
 import autoprefixer from 'autoprefixer';
 
 export default defineConfig({
+  base: '/yourbank/',
+  build: {
+    rollupOptions: {
+      input: {
+        home: resolve(__dirname, 'index.html'),
+        about: resolve(__dirname, 'about/index.html'),
+        careers: resolve(__dirname, 'careers/index.html'),
+        security: resolve(__dirname, 'security/index.html'),
+      },
+    },
+  },
   css: {
     postcss: {
       plugins: [autoprefixer({})],
